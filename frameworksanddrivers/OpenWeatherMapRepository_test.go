fa4f8b11be7931b7c03e70cd7bfea50dac5564a9
Yuta Yoshinaga <yuta.yoshinaga@gmail.com>
2020-11-01 18:23:40 +0900

    ユニットテスト環境追加

---
+++ frameworksanddrivers/OpenWeatherMapRepository_test.go
@@ -0,0 +1,44 @@
+package frameworksanddrivers
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestGetCurrentWeather GetCurrentWeatherのテスト
+func TestGetCurrentWeather(t *testing.T) {
+	err := godotenv.Load(fmt.Sprintf("../%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		t.Errorf("env file not found.")
+	}
+	openWeatherMapRepository := NewOpenWeatherMapRepository()
+	t.Run("success NewOpenWeatherMapRepository()", func(t *testing.T) {
+		if openWeatherMapRepository == nil {
+			t.Errorf("NG")
+		}
+	})
+	t.Run("failed GetCurrentWeather()", func(t *testing.T) {
+		query := ""
+		currentWeather := openWeatherMapRepository.GetCurrentWeather(query)
+		if currentWeather == nil {
+			t.Errorf("NG")
+		}
+		assert.Equal(t, 401, currentWeather.Cod)
+	})
+	t.Run("success GetCurrentWeather()", func(t *testing.T) {
+		query := ""
+		query += "q=tokyo,JP"
+		query += "&appid=" + os.Getenv("APPID")
+		query += "&lang=ja"
+		query += "&units=metric"
+		currentWeather := openWeatherMapRepository.GetCurrentWeather(query)
+		if currentWeather == nil {
+			t.Errorf("NG")
+		}
+		assert.Equal(t, 200, currentWeather.Cod)
+	})
+}
