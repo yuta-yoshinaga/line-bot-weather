17589c894d36fec635802da8f22bbaec6c5fb2da
Yuta Yoshinaga <yuta.yoshinaga@gmail.com>
2021-04-15 17:34:52 +0900

    actionsのタグ付けを修正

--- frameworksanddrivers/FactoryMethod.go
+++ frameworksanddrivers/FactoryMethod.go
@@ -18,10 +18,16 @@ func NewFactoryMethod() *FactoryMethod {
 
 // GetWeatherContller 天気情報取得メソッド
 func (factoryMethod FactoryMethod) GetWeatherContller(appid string, url string) controllers.WeatherContllerIF {
-	return controllers.NewWeatherContller(usecases.NewWeatherInteractor(NewOpenWeatherMapRepository(url), presenters.NewWeatherPresenter()), appid)
+	return controllers.NewWeatherContller(usecases.NewWeatherInteractor(NewOpenWeatherMapRepository(url),
+		presenters.NewWeatherPresenter()),
+		appid)
 }
 
 // GetWeatherLineContller 天気情報取得メソッド
 func (factoryMethod FactoryMethod) GetWeatherLineContller(appid string, url string, channelSecret string, channelToken string) controllers.WeatherLineContllerIF {
-	return controllers.NewWeatherLineContller(usecases.NewWeatherInteractor(NewOpenWeatherMapRepository(url), presenters.NewWeatherPresenter()), appid, channelSecret, channelToken)
+	return controllers.NewWeatherLineContller(usecases.NewWeatherInteractor(NewOpenWeatherMapRepository(url),
+		presenters.NewWeatherPresenter()),
+		appid,
+		channelSecret,
+		channelToken)
 }
